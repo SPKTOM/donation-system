cd125f766c345d61657229565d7ef9e4e578fe42
SPKTOM <alongkon48220@gmail.com>
2026-06-28 09:56:14 +0700

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-
+const http=require('http');
+const WebSocket=require('ws');
+const PORT=process.env.PORT||10000;
+const server=http.createServer((req,res)=>{res.writeHead(200);res.end('WebSocket Server Running');});
+const wss=new WebSocket.Server({server});
+wss.on('connection',ws=>{
+ ws.on('message',msg=>{
+  wss.clients.forEach(c=>{if(c.readyState===WebSocket.OPEN)c.send(msg.toString());});
+ });
+});
+server.listen(PORT,()=>console.log('Running',PORT));
